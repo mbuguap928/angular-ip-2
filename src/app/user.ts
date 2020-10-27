702f0e1c6afed23e5c6b3029c7897baee5c233cd
mbuguap928 <mbuguap928@gmail.com>
2020-10-27 13:37:37 +0300

    modified user.ts file

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,11 @@
 export class User {
-    constructor(public name:string, public company:string,public blog:string,public location:string,public bio:string,public repos:number,public createdat:Date,public updatedat:Date){}
+    constructor(
+        public name:string, 
+        public company:string,
+        public blog:string,
+        public location:string,
+        public bio:string,
+        public repos:number,
+        public createdate:Date,
+        public updatedate:Date){}
 }
