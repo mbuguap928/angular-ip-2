4e9781484733865a8e4e72319819cd94c500368d
mbuguap928 <mbuguap928@gmail.com>
2020-10-27 11:46:43 +0300

    added user.spec.ts and user.ts files

---
+++ src/app/user.ts
@@ -0,0 +1,3 @@
+export class User {
+    constructor(public name:string, public company:string,public blog:string,public location:string,public bio:string,public repos:number,public createdat:Date,public updatedat:Date){}
+}
