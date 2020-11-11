2c3064a0a8421ade0e16d62a1b12a785745b9287
mbuguap928 <mbuguap928@gmail.com>
2020-11-11 22:43:47 +0300

    modified routing module.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'user', component: UserComponent},
+{path: 'repository', component: RepositoryComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ReposComponent } from './repos/repos.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubComponent,
+    NavbarComponent,
+    ReposComponent,
+    NotfoundComponent,
+    UserdetailsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
