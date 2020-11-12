9b46e812d65effb611a955846e3b7d7fce1e8167
mbuguap928 <mbuguap928@gmail.com>
2020-11-12 20:37:34 +0300

    modified routing module file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserComponent } from './user/user.component';
-import { RepositoryComponent } from './repository/repository.component';
+import {  GithubComponent } from './github/github.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ReposComponent } from './repos/repos.component';
+import { NavbarComponent} from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+
 
 const routes: Routes = [
-{path: 'user', component: UserComponent},
-{path: 'repository', component: RepositoryComponent},
+{ path: 'user', component: UserdetailsComponent },
+{path: 'repository', component: ReposComponent},
+{ path: 'about', component: AboutComponent },
+{ path: '**', component: NotfoundComponent },
+{ path: '', redirectTo: '/user', pathMatch: 'full' },
+
 
 ];
 
