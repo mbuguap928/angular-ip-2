62fe57d1d44f49cead7bbf9da6c97e4b3a46422b
mbuguap928 <mbuguap928@gmail.com>
2020-11-13 13:55:37 +0300

    modified clear.services file

--- src/app/clear.service.ts
+++ src/app/clear.service.ts
@@ -46,33 +46,29 @@ export class ClearService {
     //Get my GitHub user details
     getGitHubInfo(): Observable<any> {
     return this.http.get(
-    'https://api.github.com/users/mbuguap928?access_token=' + environment.apiKey
+    environment.gitHubUrl
     );
     }
     
     //Get My Repo
     getMyGitHubRepos(): Observable<any> {
-    return this.http.get('https://api.github.com/users/mbuguap928/repos');
+    return this.http.get(environment.repositoryUrl);
     }
     
     //Get my GitHub user details using Promise
     gitHubUsersRequest() {
     interface UserApi {
-    name: String;
-    company: String;
-    blog: String;
-    location: String;
-    email: String;
-    bio: String;
-    twitter_username: String;
-    public_repos: number;
-    followers: number;
-    created_at: Date;
-    updated_at: Date;
-    avatar_url: String;
+       name:string, 
+       company:string,
+       blog:string,
+       location:string,
+       bio:string,
+       repos:number,
+       createdate:Date,
+       updatedate:Date
     }
     //The User Promise
-    let urlForUserDetailsAPI = environment.gitHubUserUrl + environment.apiKey;
+    let urlForUserDetailsAPI = environment.gitHubUrl
     console.log('urlForUserDetailsAPI: ' + urlForUserDetailsAPI);
     let gitHubUserPromise = new Promise((resolve, reject) => {
     this.http
@@ -80,30 +76,18 @@ export class ClearService {
     .toPromise()
     .then(
     (response) => {
-    // console.log('Name: ' + response.name);
-    // console.log('company: ' + response.company);
-    // console.log('blog: ' + response.blog);
-    // console.log('location: ' + response.location);
-    // console.log('email: ' + response.email);
-    // console.log('bio: ' + response.bio);
-    // console.log('twitter_username: ' + response.twitter_username);
-    // console.log('public_repos: ' + response.public_repos);
-    // console.log('followers: ' + response.followers);
-    // console.log('created_at: ' + response.created_at);
-    // console.log('updated_at: ' + response.updated_at);
-    // console.log('avatar_url: ' + response.avatar_url);
+    
     this.user.name = response.name;
     this.user.company = response.company;
     this.user.blog = response.blog;
     this.user.location = response.location;
-    this.user.email = response.email;
     this.user.bio = response.bio;
-    this.user.twitter_username = response.twitter_username;
-    this.user.public_repos = response.public_repos;
-    this.user.followers = response.followers;
-    this.user.created_at = new Date(response.created_at);
-    this.user.updated_at = new Date(response.updated_at);
-    this.user.avatar_url = response.avatar_url;
+    
+    this.user.repos = response.repos;
+    
+    this.user.createdate = new Date(response.createdate);
+    this.user.updatedate = new Date(response.updatedate);
+    
     // console.log('Userrrr: ' + this.user.avatar_url);
     resolve();
     },
@@ -112,14 +96,14 @@ export class ClearService {
     this.user.company = '';
     this.user.blog = '';
     this.user.location = '';
-    this.user.email = '';
+    
     this.user.bio = '';
-    this.user.twitter_username = '';
-    this.user.public_repos = 0;
-    this.user.followers = 0;
-    this.user.created_at = new Date();
-    this.user.updated_at = new Date();
-    this.user.avatar_url = '';
+    
+    this.user.repos = 0;
+  
+    this.user.createdate = new Date();
+    this.user.updatedate = new Date();
+    
     reject(error);
     }
     );
@@ -131,21 +115,19 @@ export class ClearService {
     //Get My Repo using Promise
     gitHubReposRequest() {
     interface ReposApi {
-    name: String;
-    fullName: String;
-    accessIsPrivate: boolean;
-    owner: String;
-    htmlUrl: String;
-    description: String;
-    url: String;
-    size: number;
-    disabled: boolean;
-    archived: boolean;
+      name:string,
+      full_name:string,
+      owner:string,
+      description:string,
+      url:string
+           
+
+      
     }
     //The Repos Promise
     let gitHubReposPromise = new Promise((success, failed) => {
     this.http
-    .get<ReposApi[]>(environment.gitHubReposUrl)
+    .get<ReposApi[]>(environment.repositoryUrl)
     .toPromise()
     .then(
     (response) => {
@@ -156,15 +138,12 @@ export class ClearService {
     // console.log('Repos: ' + JSON.stringify(this.repos));
     response.forEach((element) => {
     this.repo.name = element.name;
-    this.repo.fullName = element.fullName;
-    this.repo.accessIsPrivate = element.accessIsPrivate;
-    // this.repo.owner = element.owner.;
-    this.repo.htmlUrl = element.htmlUrl;
+    this.repo.full_name = element.full_name;
+    
+    
+    
     this.repo.description = element.description;
     this.repo.url = element.url;
-    this.repo.size = element.size;
-    this.repo.disabled = element.disabled;
-    this.repo.archived = element.archived;
     console.log('Current: ' + JSON.stringify(this.repo));
     this.repos.push(this.repo);
     });
