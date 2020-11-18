c5b57272a562df4b53da67245d4df9570ddd48a1
mbuguap928 <mbuguap928@gmail.com>
2020-11-18 13:56:40 +0300

    modified user.specs file

--- src/app/user.spec.ts
+++ src/app/user.spec.ts
@@ -1,7 +1,7 @@
 import { User } from './user';
 
 describe('User', () => {
-  it('should create an instance', () => {
-    expect(new User()).toBeTruthy();
+
+74 expect(new User()).toBeTruthy(); 
   });
 });
